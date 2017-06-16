d91b6c369a317b0c4eb8e7daae68bead9e18ecf9
keansant <keansant@gmail.com>
2017-06-17 06:01:46 +0900

    update test

--- test/puzzles/towers_test.clj
+++ test/puzzles/towers_test.clj
@@ -2,7 +2,18 @@
   (:require [clojure.test :refer :all]
             [puzzles.towers :refer :all]))
 
-
+;; test-towers
+;; This test takes a functio that solves the of Towers of Hanoi, runs it on
+;; sample data, then compares the actual and expected solutions.
+;; Tower function input:
+;; The input data corresponds to [a, b, and c] in the "let" statement of test-towers.
+;; Each tower is a map with an :id and data.
+;; Tower function output:
+;; The output is a vector of steps:
+;; [[1 a c] [2 a b] [1 c b] [3 a c] [1 b a] [2 b c] [1 a c] [4 a b] [1 c b] [2 c a] [1 b a] [3 c b] [1 a c] [2 a b] [1 c b]]
+;; To test the output, each step is applied in a loop to the tower data.
+;; The "move" function takes care of applying each step to move a disc from one tower to another.
+;; When the loop is done, the completed actual data should match the expected data.
 (defn test-towers [towers-fn]
   (let [a {:id :a :data [4 3 2 1]}
         b {:id :b :data []}
@@ -24,6 +35,9 @@
                     (if next-working
                       (recur next-working next-steps)
                       working)))]
-      (is (= actual expected))))
+    [actual expected]))
 
-(defn test-towers-rec [] (test-towers towers-rec))
+(deftest test-towers-rec
+  (testing "the recursive implementation of Towers of Hanoi"
+    (let [[actual expected] (test-towers towers-rec)]
+      (is (= actual expected)))))
