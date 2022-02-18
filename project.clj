9813f9e3a01216298a55b419401599451c7693d8
kbsant <keansant@gmail.com>
2022-02-18 10:54:28 +0900

    update dep

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://www.anketeur.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [medley/medley "1.3.0"]]
   :jvm-opts ^:replace [])
